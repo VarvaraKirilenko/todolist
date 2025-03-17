197311fe900706e1edab98a8b89dc010c8e2842f
Mahlab Shrimp <mahlabsh@student.21-school.ru>
2025-03-17 23:03:02 +0300

    second commit

--- script.js
+++ script.js
@@ -4,72 +4,40 @@ const user_input = document.getElementById("user_input");
 const done_tasks_list = document.querySelector(".done_list");
 const modal = document.querySelector(".modal");
 
-loadTasks();
-loadDoneTasks();
+loadTasksNew([['tasks', false], ['done_tasks', true]]);
 
 btn_add.addEventListener("click", addNew);
+
 document.addEventListener("keydown", function(e) {
-    if(e.key == "Enter" && document.activeElement === user_input) {
+    if(e.key === "Enter" && document.activeElement === user_input) {
         addNew()
     }
 });
 
 function addNew(){
-
     if (user_input.value != "") {
-        
-        createTask(user_input.value);
+        createTaskNew(user_input.value);
         user_input.value = "";
-        saveTasks();
-        saveDoneTasks();
+        saveTasksNew([[task_list, '.task', 'tasks'], [done_tasks_list, '.done', 'done_tasks']]);
     }
 }
 
-function createTask(taskText){
+function multiple_addClasses_helper(element_and_classes){
+    element_and_classes.forEach((element) => {
+        if (element[1]){
+            element[1].forEach((item) => 
+                element[0].classList.add(item));
+     }
+    })
+}
 
-    const new_task = document.createElement("div");
-    const new_task_text = document.createElement("div");
-    const new_buttons = document.createElement("div");
-    const new_done_button = document.createElement("button");
-    const new_delete_button = document.createElement("button");
-    const new_edit_button = document.createElement("button");
-
-    new_task.classList.add("task");
-    new_task_text.classList.add("task_text");
-    new_buttons.classList.add("buttons");
-    new_done_button.classList.add("done_task");
-    new_edit_button.classList.add("edit_task");
-    new_delete_button.classList.add("delete_task");
-
-    new_task_text.textContent = taskText;
-        
-    task_list.appendChild(new_task);
-    new_task.appendChild(new_task_text);
-    new_task.appendChild(new_buttons);
-    new_buttons.appendChild(new_done_button);
-    new_buttons.appendChild(new_delete_button);
-    new_buttons.appendChild(new_edit_button);
-
-    new_delete_button.addEventListener("click", function(){
-        task_list.removeChild(new_task);
-        saveTasks();
-        saveDoneTasks();
-    });
-    
-    new_done_button.addEventListener("click", function(){
-
-        // берем заново, в ситуации если у нас изменялась задача
-        let taskText = new_task.firstChild.textContent;
-
-        task_list.removeChild(new_task);
-        createDoneTask(taskText)
-        saveTasks();
-        saveDoneTasks();
-    });
-
-    new_edit_button.addEventListener("click", function(){
-        editTask(new_task);
-    });
+function multiple_appendChild_helper(element_and_children){
+    element_and_children.forEach((element) => {
+        if (element[1]){
+        element[1].forEach((item) => 
+        element[0].appendChild(item));
+    }
+    })
 }
 
 function editTask(new_task){
@@ -84,52 +52,33 @@ function editTask(new_task){
     const accept = document.createElement("button");
     const cancel = document.createElement("button");
 
-    modal.classList.add("modal");
-    modal.classList.add("open");
-    previous_text.classList.add("previous_text");
-    edit_area.classList.add("edit_area");
-    text_input.classList.add("text");
-    text_input.classList.add("text_edit");
-    edit_buttons.classList.add("edit_buttons");
-    accept.classList.add("common_button");
-    cancel.classList.add("common_button");
+    multiple_addClasses_helper(
+        [[modal, ["modal", "open"]], [previous_text, ["previous_text"]],
+        [edit_area, ["edit_area"]], [text_input,["text", "text_edit"]],
+        [edit_buttons, ["edit_buttons"]], [accept,["common_button"]],
+        [cancel,["common_button"]]]);
 
     text_input.setAttribute("id", "edit_input");
     text_input.placeholder = "Введите новый текст задачи...";
     accept.textContent = "Принять изменения";
     cancel.textContent = "Отменить";
     previous_text.textContent = "Изменить задачу: " + new_task.firstChild.textContent;
-    modal.appendChild(edit_area);
-    edit_area.appendChild(previous_text);
-    edit_area.appendChild(text_input);
-    edit_area.appendChild(edit_buttons);
-    edit_buttons.appendChild(accept);
-    edit_buttons.appendChild(cancel);
-
-    const edit_input = document.getElementById("edit_input");
+    
+    multiple_appendChild_helper(
+        [[modal, [edit_area]], 
+        [edit_area, [previous_text, text_input, edit_buttons]],
+        [edit_buttons, [accept, cancel]]]);
 
     accept.addEventListener("click", function() {
-        if (edit_input.value != "") {
-            new_task.firstChild.textContent = edit_input.value;
-        }
+        acceptChanges(edit_input, new_task);
         modal.removeChild(edit_area);
-        task_list.querySelectorAll("button").forEach(function(item){
-            item.disabled = false;
-        });
-        saveTasks();
         modal.classList.remove("open");
     });
 
     document.addEventListener("keydown", function(e) {
-        if(e.key == "Enter" && document.activeElement === text_input) {
-            if (edit_input.value != "") {
-                new_task.firstChild.textContent = edit_input.value;
-            }
+        if(e.key === "Enter" && document.activeElement === text_input) {
+            acceptChanges(edit_input, new_task);
             modal.removeChild(edit_area);
-            task_list.querySelectorAll("button").forEach(function(item){
-                item.disabled = false;
-            });
-            saveTasks();
             modal.classList.remove("open");
         }
     });
@@ -143,58 +92,81 @@ function editTask(new_task){
     });
 }
 
-function saveTasks(){
+function acceptChanges(edit_input, new_task){
+    if (edit_input.value != "") {
+        new_task.firstChild.textContent = edit_input.value;
+    }
+    task_list.querySelectorAll("button").forEach(function(item){
+        item.disabled = false;
+    });
+    saveTasksNew([[task_list, '.task', 'tasks']]);
+}
 
-    let tasks = [];
-    task_list.querySelectorAll(".task").forEach(function(item){
+function loadTasksNew(tasks_array){
+    tasks_array.forEach((array_element) => {
+        const tasks = JSON.parse(localStorage.getItem(array_element[0])) || [];
+        tasks.forEach((task_text => createTaskNew(task_text, array_element[1])));
+    });
+}
+
+function saveTasksNew(tasks_array){
+    tasks_array.forEach((array_element) =>
+    {
+        const tasks = [];
+        array_element[0].querySelectorAll(array_element[1]).forEach(function(item){
         tasks.push(item.firstChild.textContent.trim())
+        });
+        localStorage.setItem(array_element[2], JSON.stringify(tasks))
     });
-
-    localStorage.setItem('tasks', JSON.stringify(tasks))
 }
 
-function loadTasks(){
-    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-    tasks.forEach(createTask)
-}
-
-function saveDoneTasks(){
-
-    let done_tasks = [];
-    done_tasks_list.querySelectorAll(".done").forEach(function(item){
-        done_tasks.push(item.firstChild.textContent.trim())
-    });
-
-    localStorage.setItem('done_tasks', JSON.stringify(done_tasks))
-}
-
-function loadDoneTasks(){
-    const done_tasks = JSON.parse(localStorage.getItem('done_tasks')) || [];
-    done_tasks.forEach(createDoneTask)
-}
-
-function createDoneTask(taskText){
-    const new_done_task = document.createElement("div");
+function createTaskNew(taskText, isDone = false){
+    const new_task = document.createElement("div");
     const new_task_text = document.createElement("div");
-    new_task_text.classList.add("task_text");
-    new_done_task.classList.add("task");
-    new_done_task.classList.add("done");
-    new_task_text.textContent = taskText;
-    
     const new_buttons = document.createElement("div");
     const new_delete_button = document.createElement("button");
 
-    new_buttons.classList.add("buttons");
-    new_delete_button.classList.add("delete_task");
-        
-    done_tasks_list.appendChild(new_done_task);
-    new_done_task.appendChild(new_task_text);
-    new_done_task.appendChild(new_buttons);
-    new_buttons.appendChild(new_delete_button);
+    let new_done_button = null, new_edit_button = null;
+    let task_classes = ["task", "done"];
+    let buttons = [new_delete_button];
+    let list = done_tasks_list;
+
+    if (!isDone){
+        new_done_button = document.createElement("button");
+        new_edit_button = document.createElement("button");
+        task_classes = ["task"];
+        edit_button = [new_edit_button,["edit_task"]];
+        done_button = [new_done_button,["done_task"]];
+        list = task_list;
+        buttons = [new_done_button, new_edit_button, new_delete_button];
+    }
+
+    multiple_addClasses_helper(
+        [[new_task_text, ["task_text"]], [new_task, task_classes],
+        [new_buttons, ["buttons"]], [new_delete_button,["delete_task"]],
+        edit_button, done_button]);
+
+    new_task_text.textContent = taskText;
+
+    multiple_appendChild_helper([[list, [new_task]],
+                                 [new_task, [new_task_text, new_buttons]],
+                                 [new_buttons, buttons]]);
 
     new_delete_button.addEventListener("click", function(){
-        done_tasks_list.removeChild(new_done_task);
-        saveTasks();
-        saveDoneTasks();
-    });    
+        list.removeChild(new_task);
+        saveTasksNew([[task_list, '.task', 'tasks'], [done_tasks_list, '.done', 'done_tasks']]);
+    });   
+    
+    if (!isDone){
+        new_done_button.addEventListener("click", function(){
+            let taskText = new_task.firstChild.textContent;
+            task_list.removeChild(new_task);
+            createTaskNew(taskText, true)
+            saveTasksNew([[task_list, '.task', 'tasks'], [done_tasks_list, '.done', 'done_tasks']]);
+        });
+    
+        new_edit_button.addEventListener("click", function(){
+            editTask(new_task);
+        });
+    }
 }
